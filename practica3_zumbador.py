6a2c681c0d442062a38cf32412af7229b0ebe4a4
WilliBobadilla <willi1997.1@gmail.com>
2019-01-31 02:56:43 +0000

    Se corrigieron errores

--- practica3_zumbador.py
+++ practica3_zumbador.py
@@ -1,3 +1,7 @@
+# _*_ coding: utf-8 _*_
+
+
+
 """autor: Williams bobadilla
    fecha de creacion: 28-enero-2019
    fecha de ultima edicion: 28-enero-2019 
@@ -18,15 +22,15 @@ buzzer=25                 #conectado al gpio 25
 
 gpio.setmode(gpio.BCM) 			# modo BCM  de la raspberry pi
  
-gpio.setup(buzzer,gpio.out)       #configuramos los puertos conectador a los leds como salida
+gpio.setup(buzzer,gpio.OUT)       #configuramos los puertos conectador a los leds como salida
 
 tono=gpio.PWM(buzzer,100)        # inicializamos el objeto tono en el pin del buzzer con 
 
 tono.start(5)                    # iniciamos el tono en 5 
 while True: 
-	tono.ChangeDutyCicle(15)     # cambiamos el ciclo de trabajo
+	tono.ChangeDutyCycle(15)     # cambiamos el ciclo de trabajo
 	sleep(1)
-	tono.ChangeDutyCicle(90)
+	tono.ChangeDutyCycle(90)
 	sleep(1)
 	
 
