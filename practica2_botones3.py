3b281b2f6e9cd66e9460e7d2ec938a4aa2058ec7
WilliBobadilla <willi1997.1@gmail.com>
2019-02-01 01:45:09 +0000

    Se corrigieron errores

--- practica2_botones3.py
+++ practica2_botones3.py
@@ -1,3 +1,6 @@
+#_*_ coding: utf-8 _*_
+
+
 """autor: Williams bobadilla
    fecha de creacion: 28-enero-2019
    fecha de ultima edicion: 28-enero-2019 
@@ -22,22 +25,22 @@ entrada=22
 
 gpio.setmode(gpio.BCM) 			# modo BCM de la raspberry pi
  
-gpio.setup(led1,gpio.out)       #configuramos los puertos conectados a los leds como salida
-gpio.setup(led2,gpio.out)
-gpio.setup(led3,gpio.out)
-gpio.setup(entrada,gpio.in)     #configuramos como entrada
+gpio.setup(led1,gpio.OUT)       #configuramos los puertos conectados a los leds como salida
+gpio.setup(led2,gpio.OUT)
+gpio.setup(led3,gpio.OUT)
+gpio.setup(entrada,gpio.IN)     #configuramos como entrada
 
 contador=0                      #variable para manejo de los leds
 estado=False
 
 lista=[led1,led2,led3]          #lista que contiene los leds, para acceder a un led en especifico usar lista[index_numero_led]
 
-
 while True:                          
    if gpio.input(entrada):
       contador=contador+1               #aumentamos la variable contador
-	   if contador==4:					    #como solo tenemos 3 leds, contador debe tomar los valores 0,1,2
-	  	   contador=0				 		    #pero usamos el 3 para apagar todos los leds
+      sleep(0.3)  
+      if contador==4:					    #como solo tenemos 3 leds, contador debe tomar los valores 0,1,2
+	 contador=0				 		    #pero usamos el 3 para apagar todos los leds
    
    for a in range(len(lista)):          #un ciclo del tamaño de la lista
       if a==contador:
